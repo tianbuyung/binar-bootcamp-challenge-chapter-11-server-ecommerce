0437af26e1d6a415ed7f0a8e86d14cb2d4f116bd
florensius daniel josef <capricondaniel@gmail.com>
2022-11-02 20:42:31 +0700

    other

--- controllers/UserController.test.js
+++ controllers/UserController.test.js
@@ -1,13 +1,14 @@
 // import supertest from "supertest";
 // import App from "../../app";
-const { mockReq, mockRes } = require("../utils/interceptor");
+// const { mockReq, mockRes } = require("../utils/interceptor");
+// const jest = require("jest");
 const request = require("supertest");
 const app = require("../app");
-	const { Sequelize } = require("sequelize");
-	require("dotenv").config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 // const passport = require("passport");
-const UserController = require("../UserController");
+const UserController = require("../controllers/UserController");
 
 //? pakai supertest
 // describe("test UserController.getUserById", () => {
@@ -17,6 +18,21 @@ const UserController = require("../UserController");
 // });
 
 
+const mockReq = () => {
+	const req = {};
+	req.body = jest.fn().mockReturnValue(req);
+	req.params = jest.fn().mockReturnValue(req);
+	return req;
+}
+
+const mockRes = () => {
+	const res = {};
+	res.send = jest.fn().mockReturnValue(res);
+	res.status = jest.fn().mockReturnValue(res);
+	res.json = jest.fn().mockReturnValue(res);
+	return res;
+}
+	
 describe("coba pakai jest", () => {
 	beforeAll(async () => {
 
@@ -26,7 +42,7 @@ describe("coba pakai jest", () => {
 		null,
 		{
 			host: "localhost",
-			dialect: "mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+			dialect: "postgres" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
 		}
 	);
 	const testConnection = async () => {
@@ -46,15 +62,25 @@ describe("coba pakai jest", () => {
 		expect(response.statusCode).toBe(200);
 	});
 
-	test("with mock request and mock response", async () => {
+// 	test("with mock request and mock response", async () => {
+// 		let req = mockReq();
+// 		req.body.email = "capricondaniel@gmail.com";
+// 		req.body.password = "danieltan";
+// 		const res = mockRes();
+
+// 		await UserController.login(req, res);
+		
+// 		expect(req.send).toHaveBeenCalledTimes(1);
+// 		expect(req.status).toBeGreaterThanOrEqual(200);
+// 		done();
+// 	});
+	
+	test("get detail user", async () => {
 		let req = mockReq();
-		req.body.email = "capricondaniel@gmail.com";
-		req.body.password = "danieltan";
-		const res = mockRes();
-
-		await UserController.login(req, res);
-
-		expect(req.send).toHaveBeenCalledTimes(1);
-		expect(req.status).toBeGreaterThanOrEqual(200);
-	});
+		req.authotization = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNhcHJpY29uZGFuaWVsQGdtYWlsLmNvbSIsImlhdCI6MTY2NzM4MTkyOSwiZXhwIjoxNjY3Mzg1NTI5fQ.y2QMTaYuDkAU2ZrcpbwfGMu0qDr4y7fwArerIgoxrZE";
+		let res = mockRes();
+		const response = await UserController.getUserById(req, res);
+		console.log("cek response = ", response);
+		expect(response.statusCode).toBe(200);
+	})
 });
