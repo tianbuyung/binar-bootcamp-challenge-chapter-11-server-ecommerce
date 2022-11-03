c609a7beb288ecbea701be2e0674c091c5e1c572
Septian Maulana <septianm028@gmail.com>
2022-11-03 21:44:11 +0700

    [tianbuyung] - added 1 positive and 2 negative test, edited eslint setting for jest, and refactor testing variable

--- controllers/AdminController.spec.js
+++ controllers/AdminController.spec.js
@@ -1,16 +1,16 @@
-/* eslint-disable */
-/* eslint-disable no-undef */
-const AdminController = require("./AdminController");
 const request = require("supertest");
 const app = require("../app");
-const Model = require("../models");
-const { Admin } = Model;
+
+const email = "Keven_West99@yahoo.com";
+const password = "binarecommerce";
+const jwtToken =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IktldmVuX1dlc3Q5OUB5YWhvby5jb20iLCJpYXQiOjE2Njc0ODU2NDcsImV4cCI6MTY2NzQ4OTI0N30.EiaOANiC6Aknr6vRsY5X_4ekAWZkuGS3f1rZUSbABX8";
 
 describe("POST /admin", () => {
-	test(`Return status 200 and login get token`, (done) => {
+	test("Return status 200 and login get token", (done) => {
 		request(app)
 			.post("/admin")
-			.send({ email: "Keven_West99@yahoo.com", password: "binarecommerce" })
+			.send({ email, password })
 			.set("Accept", "application/json")
 			.expect("Content-Type", /json/)
 			.then((res) => {
@@ -20,10 +20,10 @@ describe("POST /admin", () => {
 				done();
 			});
 	});
-	test(`Return status 401 and email is wrong`, (done) => {
+	test("Return status 401 and email is wrong", (done) => {
 		request(app)
 			.post("/admin")
-			.send({ email: "Keve_West99@yahoo.com", password: "binarecommerce" })
+			.send({ email: "wrongEmail", password })
 			.set("Accept", "application/json")
 			.expect("Content-Type", /json/)
 			.then((res) => {
@@ -32,15 +32,52 @@ describe("POST /admin", () => {
 				done();
 			});
 	});
-	test(`Return status 401 and password is wrong`, (done) => {
+	test("Return status 401 and password is wrong", (done) => {
 		request(app)
 			.post("/admin")
-			.send({ email: "Keven_West99@yahoo.com", password: "binarecommerc" })
+			.send({ email, password: "wrongPassword" })
 			.set("Accept", "application/json")
 			.expect("Content-Type", /json/)
 			.then((res) => {
 				expect(res.statusCode).toBe(401);
 				expect(res.body).toHaveProperty("message", "Wrong email or password");
+				done();
+			});
+	});
+	test("Return status 500 and error", (done) => {
+		request(app)
+			.post("/admin")
+			.send({})
+			.set("Accept", "application/json")
+			.expect("Content-Type", /json/)
+			.then((res) => {
+				expect(res.statusCode).toBe(500);
+				expect(res.body).toHaveProperty("message");
+				done();
+			});
+	});
+});
+
+describe("GET /verify", () => {
+	test("Return status 200 and authorized", (done) => {
+		request(app)
+			.get("/admin/verify")
+			.set({ Authorization: jwtToken, Accept: "application/json" })
+			.expect("Content-Type", /json/)
+			.then((res) => {
+				expect(res.statusCode).toBe(200);
+				expect(res.body).toHaveProperty("message");
+				done();
+			});
+	});
+	test("Return status 403 and unauthorized", (done) => {
+		request(app)
+			.get("/admin/verify")
+			.set({ Authorization: "wrongToken", Accept: "application/json" })
+			.expect("Content-Type", /json/)
+			.then((res) => {
+				expect(res.statusCode).toBe(403);
+				expect(res.body).toHaveProperty("message");
 				done();
 			});
 	});
