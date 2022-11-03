3abce96bf5a3b0685ea20a9a4915c8e774043dd5
Septian Maulana <septianm028@gmail.com>
2022-11-03 20:54:43 +0700

    [tianbuyung] - added 2 negative testing for admin controller

--- controllers/AdminController.spec.js
+++ controllers/AdminController.spec.js
@@ -7,18 +7,41 @@ const Model = require("../models");
 const { Admin } = Model;
 
 describe("POST /admin", () => {
-  test(`Return status 200 and login get token`, (done) => {
-    request(app)
-      .post("/admin")
-      .send({ email: "Keven_West99@yahoo.com", password: "binarecommerce" })
-      .set("Accept", "application/json")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((res) => {
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toHaveProperty("message");
-        expect(res.body).toHaveProperty("token");
-        done();
-      });
-  });
+	test(`Return status 200 and login get token`, (done) => {
+		request(app)
+			.post("/admin")
+			.send({ email: "Keven_West99@yahoo.com", password: "binarecommerce" })
+			.set("Accept", "application/json")
+			.expect("Content-Type", /json/)
+			.then((res) => {
+				expect(res.statusCode).toBe(200);
+				expect(res.body).toHaveProperty("message");
+				expect(res.body).toHaveProperty("token");
+				done();
+			});
+	});
+	test(`Return status 401 and email is wrong`, (done) => {
+		request(app)
+			.post("/admin")
+			.send({ email: "Keve_West99@yahoo.com", password: "binarecommerce" })
+			.set("Accept", "application/json")
+			.expect("Content-Type", /json/)
+			.then((res) => {
+				expect(res.statusCode).toBe(401);
+				expect(res.body).toHaveProperty("message", "Wrong email or password");
+				done();
+			});
+	});
+	test(`Return status 401 and password is wrong`, (done) => {
+		request(app)
+			.post("/admin")
+			.send({ email: "Keven_West99@yahoo.com", password: "binarecommerc" })
+			.set("Accept", "application/json")
+			.expect("Content-Type", /json/)
+			.then((res) => {
+				expect(res.statusCode).toBe(401);
+				expect(res.body).toHaveProperty("message", "Wrong email or password");
+				done();
+			});
+	});
 });
