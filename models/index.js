897f6a847eb1f71e68e26d5f7892de6e8589d667
akurnia1 <akurnia@binaracademy.org>
2022-11-04 21:07:02 +0700

    set db

--- models/index.js
+++ models/index.js
@@ -4,20 +4,31 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.js")[env];
+// const env = process.env.NODE_ENV || "development";
+// const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-	sequelize = new Sequelize(
-		"postgres://gaxgayvkhhceks:b225b01699e5854f77692b59f73e05ce34613d91784dd5337ed6f5cd24074583@ec2-54-163-34-107.compute-1.amazonaws.com:5432/ddmtgvv0s2v3ob"
-	);
-} else {
-	sequelize = new Sequelize(
-		"postgres://gaxgayvkhhceks:b225b01699e5854f77692b59f73e05ce34613d91784dd5337ed6f5cd24074583@ec2-54-163-34-107.compute-1.amazonaws.com:5432/ddmtgvv0s2v3ob"
-	);
-}
+// let sequelize;
+// if (config.use_env_variable) {
+// 	sequelize = new Sequelize(
+// 		"postgres://gaxgayvkhhceks:b225b01699e5854f77692b59f73e05ce34613d91784dd5337ed6f5cd24074583@ec2-54-163-34-107.compute-1.amazonaws.com:5432/ddmtgvv0s2v3ob"
+// 	);
+// } else {
+// 	sequelize = new Sequelize(
+// 		"postgres://gaxgayvkhhceks:b225b01699e5854f77692b59f73e05ce34613d91784dd5337ed6f5cd24074583@ec2-54-163-34-107.compute-1.amazonaws.com:5432/ddmtgvv0s2v3ob"
+// 	);
+// }
+// Option 2: Passing parameters separately (sqlite)
+const sequelize = new Sequelize(
+	"ddmtgvv0s2v3ob",
+	"gaxgayvkhhceks",
+	"b225b01699e5854f77692b59f73e05ce34613d91784dd5337ed6f5cd24074583",
+	{
+		dialect: "postgres",
+		port: "5432",
+		host: "ec2-54-163-34-107.compute-1.amazonaws.com",
+	}
+);
 
 fs.readdirSync(__dirname)
 	.filter((file) => {
