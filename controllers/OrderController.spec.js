f1047c40c31ad761ad8a232cceac56369f7f4b78
rikyyang <yang.riky@gmail.com>
2022-11-07 14:52:53 +0700

    Riky - testing order

---
+++ controllers/OrderController.spec.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+const request = require("supertest");
+const app = require("../app");
+const jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ik1heGltZTk2QHlhaG9vLmNvbSIsImlhdCI6MTY2NzU0Nzk2OCwiZXhwIjoxNjY3NTUxNTY4fQ.fbGL9F8S-OTK-QeXrYSZieF9vFM8ojG7mMUJBD8B61U";
+
+describe("CREATE ORDER /orders", () => {
+    test("Return status 200 and cart details", (done) => {
+        request(app)
+            .post("/orders")
+            .send({ "totalOrder": 1000 })
+            .set({ Authorization: jwtToken, Accept: "application/json" })
+            .expect("Content-Type", /json/)
+            .then((res) => {
+                expect(res.statusCode).toBe(200);
+                expect(res.body).toHaveProperty("message");
+                expect(res.body).toHaveProperty("data");
+                done();
+            });
+    });
+});
+
+describe("GET /verify", () => {
+    test("Return status 200 and authorized", (done) => {
+        request(app)
+            .get("/admin/verify")
+            .set({ Authorization: jwtToken, Accept: "application/json" })
+            .expect("Content-Type", /json/)
+            .then((res) => {
+                expect(res.statusCode).toBe(200);
+                expect(res.body).toHaveProperty("message");
+                done();
+            });
+    });
+});
